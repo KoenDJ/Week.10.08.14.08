f06c62baadbedf9260b24d375db558368416c3c2
Koen De Jans <dejanskoen@gmail.com>
2020-08-10 09:15:16 +0200

    Hello World toegevoegd

---
+++ Week.10.08.14.08/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week._10._08._14._08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
